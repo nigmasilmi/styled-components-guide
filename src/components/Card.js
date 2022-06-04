09b2ccf23ff8bfea9fa701537e9ebd4e35aca9e6
Nigma Silmi <sn.silmi@gmail.com>
2022-06-04 16:53:40 -0400

    making a card: media queries, pseudo-elementspseudo-selectors

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ const Card = () => {
       <img
         src="https://dl.airtable.com/.attachmentThumbnails/a6119fabf7256049cc0e8dbcdf536c9c/b0153f66?ts=1654373137&userId=usrQMwWEPx18KgLcP&cs=dfa2f910968a77eb"
         alt="product"
-      ></img>
+      />
       <footer>
         <h4>Product name</h4>
         <p>$15</p>
@@ -22,6 +22,7 @@ const StyledCard = styled.article`
   max-width: 300px;
   background-color: #fff;
   border-radius: 0.25rem;
+
   img {
     width: 100%;
     border-top-left-radius: 0.25rem;
@@ -33,13 +34,31 @@ const StyledCard = styled.article`
     align-items: center;
     padding: 0 2rem;
     h4 {
+      padding: 0.25rem 0.4rem;
+      border-radius: 0.15rem;
       text-transform: capitalize;
       color: var(--primary);
+      &::before {
+        content: "Name: ";
+      }
+    }
+    /* hover on element */
+    h4:hover {
+      background-color: #edd9d9;
     }
     p {
       color: var(--secondary);
       font-weight: 700;
     }
+    /* tansition on container */
+    transition: all 0, 5s ease-in-out;
+    /* hover on container */
+    &:hover {
+      box-shadow: 0 3px 3px #222;
+    }
+  }
+  @media (min-width: 768px) {
+    max-width: 600px;
   }
 `;
 
